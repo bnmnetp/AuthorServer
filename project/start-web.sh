1f05451e85b0cab3a65a892fdd4910d443d02d91
Brad Miller <bonelake@mac.com>
2022-08-17 14:35:52 -0500

    fix poetry install

--- project/start-web.sh
+++ project/start-web.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [ ! -f .web-installed ]
-then
-    poetry install
-    touch .web-installed
-fi
 poetry run uvicorn main:app --host 0.0.0.0 --reload
 tail -f /var/log/lastlog
--- project/start-worker.sh
+++ project/start-worker.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-if [ ! -f .worker-installed ]
-then
-    poetry install
-    touch .worker-installed
-fi
 poetry run celery -A worker.celery worker
 tail -f /var/log/lastlog
